74015a0146e9c52b5579d16a1a0efcba2145a94f
SpikyWU7 <spikywu7@gmail.com>
2022-05-31 17:17:04 +0300

    Scroll View

---
+++ SpaceXRockets/SpaceXRockets/View/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceXRockets
+//
+//  Created by Spiky WU7 on 30.05.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
