754cf7919c138618cb37177c383408681c1173c5
SpikyWU7 <spikywu7@gmail.com>
2022-06-01 15:02:21 +0300

    Horizontal scroll view/ main interface done
    Tested on ipod 7, 12 max, 12 mini

--- SpaceXRockets/SpaceXRockets/View/MainViewController.swift
+++ SpaceXRockets/SpaceXRockets/View/MainViewController.swift
@@ -9,12 +9,22 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    
+    @IBOutlet var hsvHeightView: UIView!
+    @IBOutlet var hsvDiameterView: UIView!
+    @IBOutlet var hsvMassView: UIView!
+    @IBOutlet var hsvPayloadLb: UIView!
+    
     @IBOutlet var scrollViewInfo: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         scrollViewInfo.layer.cornerRadius = 30.0
+        hsvMassView.layer.cornerRadius = 30.0
+        hsvPayloadLb.layer.cornerRadius = 30.0
+        hsvHeightView.layer.cornerRadius = 30.0
+        hsvDiameterView.layer.cornerRadius = 30.0
     }
 
 
